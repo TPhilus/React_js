7ab5edb540b5edebf30333670881a2493a7bd97e
Theophilus K. Dadzie <tkdadzie@st.ug.edu.gh>
2023-04-24 09:38:40 -0400

    adding useState(Hooks)

--- src/components/Expenses/Expenseltem.jsx
+++ src/components/Expenses/Expenseltem.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => { 
 
+  const clickHandler = () => {
+    console.log('Clicked!');
+  };
   
   return (
     <Card  className="expense-item">
@@ -13,6 +16,7 @@ const ExpenseItem = (props) => {
         <h2> {props.title} </h2> 
         <div className="expense-item__price">${props.amount}</div>
       </div> 
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ExpenseItem from "./Expenseltem";
 import './Expenses.css';
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
   return (
